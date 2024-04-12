4785a4ec361e78c6a218edd01927c5c34b08952c
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-04-11 22:23:10 -0300

    .

--- service_aula1104/src/app/components/cliente/cliente.component.ts
+++ service_aula1104/src/app/components/cliente/cliente.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Cliente } from '../../interfaces/cliente';
+import { ClienteService } from './../../services/cliente.service';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
+  clientes:Cliente[]=[]
+  constructor(private clienteService:ClienteService){
+  }
 
+  listar():void{
+    this.clientes = this.clienteService.listar()//fez uma lista cliente para recebe as infos da api e o front usa essa lista cliente
+  }
+
+  ngOnInit():void{//ngOnInit() --> quando inicia a página ele chama esse método listar desse clientCoponent
+    this.listar()
+  }
 }
