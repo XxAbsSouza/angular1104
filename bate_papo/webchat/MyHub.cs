b61292f266d117ef055f72fd09e65b6772907147
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-05-09 20:55:33 -0300

    .

---
+++ bate_papo/webchat/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace webchat
+{
+    public class MyHub : Hub
+    //o Hub encapsula o websocket
+    {
+       public async Task EnviarMensagem(string mensagem)
+        {
+            await Clients.All.SendAsync("ReceberMensagem", mensagem);
+        }
+    }
+}
