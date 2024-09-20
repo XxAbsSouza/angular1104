10ffa9af22dfef9d634b563927bd728144c5d2aa
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-09-19 22:16:34 -0300

    .

---
+++ WebApIPerformace/WebApIPerformace/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using WebApIPerformace.Model;
+
+namespace WebApIPerformace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetUsuario()
+        {
+       
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            String query = "Select Id, Nome, Email from Usuarios;";
+            var usuarios = await connection.QueryAsync<Usuario>(query);
+
+            return Ok(usuarios);
+        }
+    }
+}
